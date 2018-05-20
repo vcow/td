bdd2b090277cf60be8fd73320c56f5570dea4fcf
Yacow Vircowskiy <vcow@yandex.ru>
2018-05-20 20:52:04 +0300

    enemy motion

--- project/Assets/Scripts/AI/EnemyLogic.cs
+++ project/Assets/Scripts/AI/EnemyLogic.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Models.Enemies;
 using UnityEngine;
 
@@ -9,22 +10,84 @@ namespace AI
         private readonly IEnemy _enemy;
         private readonly IEnumerable<Vector3> _path;
         private readonly IGameLogic _gameLogic;
-        
+
+        private int _health;
+        private Vector3 _calcPosition;
+        private float _totalTime;
+        private bool _targetReached;
+
         public EnemyLogic(IEnemy enemy, IEnumerable<Vector3> path, IGameLogic gameLogic)
         {
             _enemy = enemy;
             _path = path;
             _gameLogic = gameLogic;
+
+            _health = enemy.Health;
+            _calcPosition = _path.FirstOrDefault();
+            _totalTime = 0;
         }
-        
+
         public IEnemy Model
         {
             get { return _enemy; }
         }
-        
+
         public void Update(float deltaTime)
         {
-            // TODO:
+            _totalTime += deltaTime;
+
+            if (_health <= 0 || _targetReached) return;
+            
+            bool targetReached;
+            _calcPosition = DoCalcPosition(_totalTime, out targetReached);
+
+            if (targetReached)
+            {
+                // Win
+                _gameLogic.HitTarget(_enemy.Damage);
+                _targetReached = true;
+            }
+        }
+
+        public int Health
+        {
+            get { return _health; }
+        }
+
+        /// <summary>
+        /// Получить рассчетное положение врага.
+        /// </summary>
+        /// <param name="extraTime">Смещение по времени, позволяет рассчитывать позицию врага
+        /// в которой он окажется через указанное время.</param>
+        /// <returns>Рассчетная позиция.</returns>
+        public Vector3 GetPosition(float extraTime = 0)
+        {
+            bool targetReached;
+            return Mathf.Approximately(extraTime, 0)
+                ? _calcPosition
+                : DoCalcPosition(_totalTime + extraTime, out targetReached);
+        }
+
+        private Vector3 DoCalcPosition(float time, out bool targetReached)
+        {
+            var speed = _enemy.Speed;
+            var index = Mathf.FloorToInt(time / speed);
+            if (index >= _path.Count() - 1)
+            {
+                targetReached = true;
+                return _path.LastOrDefault();
+            }
+
+            targetReached = false;
+            if (index <= 0 && Mathf.Approximately(time, 0))
+            {
+                return _path.FirstOrDefault();
+            }
+
+            var stepFrom = _path.ElementAt(index);
+            var stepTo = _path.ElementAt(index + 1);
+            var k = Mathf.Clamp01((time - speed * index) / speed);
+            return Vector3.Lerp(stepFrom, stepTo, k);
         }
     }
 }
--- project/Assets/Scripts/Views/Enemies/EnemyView.cs
+++ project/Assets/Scripts/Views/Enemies/EnemyView.cs
@@ -1,10 +1,42 @@
 ﻿using AI;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Views.Enemies
 {
     public class EnemyView : MonoBehaviour
     {
+        private Transform _transform;
+        private bool _isDead;
+        
         public EnemyLogic Logic { set; private get; }
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void LateUpdate()
+        {
+            Assert.IsNotNull(Logic);
+            if (_isDead) return;
+
+            _isDead = Logic.Health <= 0;
+            if (_isDead)
+            {
+                Die();
+            }
+            else
+            {
+                transform.localPosition = Logic.GetPosition();
+            }
+        }
+
+        private void Die()
+        {
+            // TODO: Animate effect for the death.
+            
+            Destroy(gameObject);
+        }
     }
 }
